550f00c2939ba42eadfde2ebf9b2e5643935a7b0
SillyInventor <SillyInventor@gmail.com>
2017-06-16 10:34:36 -0400

    Initial setup

---
+++ example-block.js
@@ -0,0 +1,8 @@
+example('test');
+
+/** This is an example function
+@param {any} input - takes anything as input
+@return {any} output - returns input*/
+function example(input) {
+      return input;
+}
