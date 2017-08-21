f0434f4ffe3d8387c85a1962cd972b2ff354a0ec
Han Hai <haih2@rpi.edu>
2017-08-20 22:11:04 -0400

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,25 @@
 module.exports = {
-    "extends": "google"
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
 };
