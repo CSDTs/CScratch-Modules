d6336449e46832d06856dc23bd0f81b1c15c9865
Jake Bartles <bartljak@gmail.com>
2017-10-17 14:59:39 -0400

    added first block

---
+++ 3D/3D.js
@@ -0,0 +1,18 @@
+let NEW_BLOCK_STRING_rotate = '<block type="motion_rotate" id="motion_rotate">'
++'<value name="DEGREESX"> <shadow type="math_number"><field name="NUM">0</field>'
++'</shadow></value><value name="DEGREESY"><shadow type="math_number">'
++'<field name="NUM">0</field></shadow></value><value name="DEGREESZ">'
++'<shadow type="math_number"><field name="NUM">0</field></shadow></value></block>'
+
+vm.runtime._primitives.motion_rotate = function (args, util) {
+    let x = Number(args.NUM_X);
+    let y = Number(args.NUM_Y);
+    let z = Number(args.NUM_Z);
+}
+
+let toolboxDOM = (new DOMParser).parseFromString(ScratchBlocks.Blocks.defaultToolbox, 'text/xml')
+let newBlockDOM_rotate = (new DOMParser).parseFromString(NEW_BLOCK_STRING_rotate, 'text/xml')
+
+toolboxDOM.getElementsByName('motion')[0].appendChild(newBlockDOM_rotate.children[0])
+let toolboxXML = (new XMLSerializer).serializeToString(toolboxDOM)
+Blockly.getMainWorkspace().updateToolbox(toolboxXML)
