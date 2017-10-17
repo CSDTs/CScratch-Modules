8a4c7b3eaaf2a097d521881bf4fa42f0a774780c
Jake Bartles <bartljak@gmail.com>
2017-10-17 15:20:32 -0400

    added another block

--- 3D/3D.js
+++ 3D/3D.js
@@ -4,6 +4,12 @@ let NEW_BLOCK_STRING_rotate = '<block type="motion_rotate" id="motion_rotate">'
 +'<field name="NUM">0</field></shadow></value><value name="DEGREESZ">'
 +'<shadow type="math_number"><field name="NUM">0</field></shadow></value></block>'
 
+let NEW_BLOCK_STRING_setrotation = '<block type="motion_setrotation" id="motion_setrotation">'
++'<value name="DEGREESX"><shadow type="math_number"><field name="NUM">0</field>'
++'</shadow></value><value name="DEGREESY"><shadow type="math_number"><field name="NUM">0</field>'
++'</shadow></value><value name="DEGREESZ"><shadow type="math_number"><field name="NUM">0</field>'
++'</shadow></value></block>'
+
 vm.runtime._primitives.motion_rotate = function (args, util) {
     let x = Number(args.NUM_X);
     let y = Number(args.NUM_Y);
@@ -12,7 +18,9 @@ vm.runtime._primitives.motion_rotate = function (args, util) {
 
 let toolboxDOM = (new DOMParser).parseFromString(ScratchBlocks.Blocks.defaultToolbox, 'text/xml')
 let newBlockDOM_rotate = (new DOMParser).parseFromString(NEW_BLOCK_STRING_rotate, 'text/xml')
+let newBlockDOM_setrotation = (new DOMParser).parseFromString(NEW_BLOCK_STRING_setrotation, 'text/xml')
 
 toolboxDOM.getElementsByName('motion')[0].appendChild(newBlockDOM_rotate.children[0])
+toolboxDOM.getElementsByName('motion')[0].appendChild(newBlockDOM_setrotation.children[0])
 let toolboxXML = (new XMLSerializer).serializeToString(toolboxDOM)
 Blockly.getMainWorkspace().updateToolbox(toolboxXML)
