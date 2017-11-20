f8717704539a68b0d48f2752d043f7115ccc95cf
Silly Inventor <SillyInventor@users.noreply.github.com>
2017-11-20 12:56:28 -0500

    Fixing Han's Mistaken linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,4 @@ module.exports = {
         "es6": true
     },
     "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
 };
